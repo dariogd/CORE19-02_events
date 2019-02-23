c91035c278d4a34453be7e6210ba03519ad4a4b7
Darío Guijarro Diéguez <dario.guijarro.dieguez@gmail.com>
2019-02-23 21:22:01 +0100

    tareas

--- practica2.js
+++ practica2.js
@@ -2,6 +2,7 @@
 const Habitacion = require('./habitacion');
 const Climatizador = require('./climatizador');
 const Termostato = require('./termostato');
+const Programador = require('./programador');
 
 // Creamos una habitacion:
 const dormitorio = new Habitacion();
@@ -13,16 +14,35 @@ const climatizador = new Climatizador(dormitorio);
 // Creamos un Termostato que mira la temperatura de la habitacion:
 const termostato = new Termostato(dormitorio);
 
+// programador
+const configProgramador = [
+  { hora: "07:00",
+    temperatura: 22
+  },
+  { hora: "08:30",
+    temperatura: 18
+  },
+  { hora: "18:00",
+    temperatura: 22
+  },
+  { hora: "21:14",
+    temperatura: 27
+}
+];
+const programador = new Programador(configProgramador);
+
+
+
 // Configuramos el termostato para controlar la temperatura:
 termostato.on('muchofrio', () => climatizador.calentar());
 termostato.on('muchocalor', () => climatizador.enfriar());
 
 // Mostar la temperatura periodicamente:
 termostato.on('tic', (temp) => console.log(`${temp.toFixed(1)}ºC`));
+programador.on('ideal', (tempIdeal) =>  termostato.indicarTemperaturaIdeal(tempIdeal));
 
 // Configurar la temp ideal a 20 grados:
 termostato.indicarTemperaturaIdeal(20);
 
 // Encender el termostato:
 termostato.encender();
-
